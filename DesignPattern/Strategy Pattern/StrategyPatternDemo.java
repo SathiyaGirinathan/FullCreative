3c088a7eb84ac46785604d57b1402dd566ebe52c
SathiyaGirinathan <65302254+SathiyaGirinathan@users.noreply.github.com>
2021-08-21 10:49:03 +0530

    Add files via upload

---
+++ DesignPattern/Strategy Pattern/StrategyPatternDemo.java
@@ -0,0 +1,8 @@
+
+public class StrategyPatternDemo {
+	public static void main(String[] args)
+	{
+		Context context = new Context(new OperationAdd());
+		System.out.println("10+5= "+context.executeStrategy(10, 5));
+	}
+}
