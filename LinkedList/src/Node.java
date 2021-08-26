7832b2b701dfd693dc187937e2714e17be13e39a
Sathiya <sathiyagirinathan@gmail.com>
2021-08-26 13:36:58 +0530

    Committed

--- LinkedList/src/Node.java
+++ LinkedList/src/Node.java
@@ -6,4 +6,22 @@ public class Node {
     {
         this.data=data;
     }
+
+    public void setData(int data)
+    {
+        this.data=data;
+    }
+
+    public int getData()
+    {
+        return data;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getNext() {
+        return this.next;
+    }
 }
